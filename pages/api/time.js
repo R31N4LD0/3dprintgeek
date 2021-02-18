46fb17c6fd23de4625fe6eb582ad1c156721ee01
Reinaldo Miranda <reinaldoap.miranda@gmail.com>
2021-02-18 00:51:46 -0300

    Pesquisa nome de pokemon

--- pages/api/time.js
+++ pages/api/time.js
@@ -1,9 +1,15 @@
-function time (request, response) {
+async function time (request, response) {
     const dynamicDate = new Date();
+    const pokemonCall = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+    const pokemonJSON = await pokemonCall.json();
+    const pokemonName = pokemonJSON.name;
 
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        pokemonName
     });
 }
+
+// https://pokeapi.co/api/v2/pokemon/1
 
 export default time;
