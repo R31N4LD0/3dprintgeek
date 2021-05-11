4252fdd40b8b2860f8bcf25f6a063dbd59d50da3
Reinaldo Miranda <reinaldoap.miranda@gmail.com>
2021-05-10 21:13:00 -0300

    Altera temporariamente os botões de paginação

--- pokemon/src/components/Pagination.js
+++ pokemon/src/components/Pagination.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import ButtonGroupIcon from '@material-ui/core/ButtonGroup';
+import CallSplitIcon from '@material-ui/icons/CallSplit';
+import FingerprintIcon from '@material-ui/icons/Fingerprint';
 
 export default function Pagination({ loadNextPage, loadPrevPage, loadDivisor }) {
     return (
-        <div>
-            <Button
-                onClick={loadDivisor}
-                size="small" 
-                variant="contained" 
-                color="secondary">
-                Divisor
-            </Button>
-            <Button
-                onClick={loadPrevPage}
-                disabled={!loadPrevPage}
-                size="small" 
-                variant="contained" 
-                color="primary">
-                Prev
-            </Button>
-            {loadNextPage && <Button
-                onClick={loadNextPage}
-                disabled={!loadNextPage}
-                size="small" 
-                variant="contained" 
-                color="primary">
-                Next
-            </Button>}
-        </div>
+        <>
+            <ButtonGroupIcon>
+                <Button
+                    startIcon={<CallSplitIcon />}
+                    endIcon={<FingerprintIcon />}
+                    onClick={loadDivisor}
+                    size="small" 
+                    variant="contained" 
+                    color="secondary">
+                    Divisor
+                </Button>
+                <Button
+                    onClick={loadPrevPage}
+                    disabled={!loadPrevPage}
+                    size="small" 
+                    variant="contained" 
+                    color="primary">
+                    Prev
+                </Button>
+                {loadNextPage && <Button
+                    onClick={loadNextPage}
+                    disabled={!loadNextPage}
+                    size="small" 
+                    variant="contained" 
+                    color="primary">
+                    Next
+                </Button>}
+            </ButtonGroupIcon>
+        </>
     )
 }
